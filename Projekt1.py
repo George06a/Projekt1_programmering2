46d32e34ebb5889938b087cdb9170f00d0fe5da2
George06a <ggeorgos43@gmail.com>
2025-11-02 13:07:09 +0100

    La till vad som händer när man har tagit skada

--- Projekt1.py
+++ Projekt1.py
@@ -2,7 +2,17 @@
 
 #Superklassen
 class Karaktärer:
-    def __init__(self,namn,hälsa,skada):
+    def __init__(self,namn,hälsa,power):
         self.namn = namn
         self.hälsa = hälsa
-        self.skada = skada
+        self.skada = power
+    
+    def attack(self, motståndare):
+        print(f"{self.namn} attackerar {motståndare.namn}")
+        motståndare.Ta_Skada(self.power)
+    
+    def Ta_Skada(self, skada):
+        self.hälsa -= skada
+        if self.hälsa < 0:
+            self.hälsa = 0
+        print(f"{self.namn} tar {skada} skada.\n Hälsa kvar: {self.hälsa}")
